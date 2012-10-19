c8f954f54413b390754dba1f8c2a4f4471564fd4
Adam Bahlke <avbdeclarus@gmail.com>
2012-10-19 10:36:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CountryTravel::Application.config.secret_token = '1f26f296945056b0ac33f059b1b446cecd01952d255b85b21a8233cef9bb1e319f86e0fcf5efcae8f55631dcef21dbbe4d660b37aed5babb3f6a45ae3ec86668'
